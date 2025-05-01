ee315ff2afa2053511d2e275bb907d5e0b932049
AlfTokenEth <info@alftoken.org>
2025-05-01 21:07:14 +0200

    Update TaxManager.sol

--- contracts/TaxManager.sol
+++ contracts/TaxManager.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+// TaxManager.sol
+pragma solidity ^0.8.19;
 
+import "./RoleManager.sol";
+
+contract TaxManager is RoleManager {
+    address public taxAccount; // Tax account address
+
+    constructor(address _taxAccount) {
+        require(_taxAccount != address(0), "Tax account cannot be the zero address");
+        taxAccount = _taxAccount; // Tax account address is deployer address by default
+    }
+    function setTaxAccount(address _taxAccount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_taxAccount != address(0), "Tax account cannot be the zero address");
+        taxAccount = _taxAccount;
+    }
+
+    function calculateTax(uint256 amount) public pure returns (uint256) {
+        return amount / 100;
+    }
+}
